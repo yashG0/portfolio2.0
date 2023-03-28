f19961d2bd79ad21303be54fdb4d21bc9101ebf8
yashG0 <yashgaurkar0@gmail.com>
2023-03-29 05:00:12 +0530

    Initial commit

--- components/Services.jsx
+++ components/Services.jsx
@@ -42,7 +42,7 @@ const Services = () => {
 
     return (
         <>
-            <PageSection title={'Services🛠️'} subTitle={'The Best Services, I have made For My Clients With Best Projects As a Preview! That Will Blow Your Mind.'}>
+            <PageSection title={'Services🛠️'} subTitle={'The Best Services, I have made For My Clients With Best Projects As a Preview! That Will Blow Your Mind'}>
 
                 <div className='grid gap-10 lg:grid-cols-2'>
 
